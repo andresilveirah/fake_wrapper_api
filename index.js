4fb5367ee3fa64a34ab2b34934fc704a1252d3e2
Andre Herculano <andresilveirah@gmail.com>
2019-12-15 17:42:54 +0100

    pass requests data into response

--- index.js
+++ index.js
@@ -12,14 +12,16 @@ app.use(logger)
 app.get('/message', (req, res) => {
   res.status(200).json({
     url: 'https://notice.sp-prod.net/?message_id=66281',
-    uuid: 'cfa454f8-635b-43e5-b6ba-1fbff7e56fa9'
+    uuid: 'cfa454f8-635b-43e5-b6ba-1fbff7e56fa9',
+    ...req.query
   })
 })
 
 app.post('/action/:type', (req, res) => {
   res.status(200).json({
     uuid: 'cfa454f8-635b-43e5-b6ba-1fbff7e56fa9',
-    euconsent: 'BOnf5gkOnf5gkAGABBENCm-AAAAqWADABUADQAUg'
+    euconsent: 'BOnf5gkOnf5gkAGABBENCm-AAAAqWADABUADQAUg',
+    ...req.body
   })
 })
 
