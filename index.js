d40e46123cf143336ccee585aa0d487c081371b2
André Herculano <andresilveirah@gmail.com>
2020-01-09 15:02:37 +0100

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express()
 app.use(json())
 app.use(logger)
 
-app.get('gdpr/message-url', (req, res) => {
+app.get('/gdpr/message-url', (req, res) => {
   res.status(200).json({
     url: "https://notice.sp-prod.net/?message_id=70171",
     uuid: 'cfa454f8-635b-43e5-b6ba-1fbff7e56fa9',
@@ -24,7 +24,7 @@ app.get('gdpr/message-url', (req, res) => {
   })
 })
 
-app.post('gdpr/consent/:type', (req, res) => {
+app.post('/gdpr/consent/:type', (req, res) => {
   res.status(200).json({
     uuid: 'cfa454f8-635b-43e5-b6ba-1fbff7e56fa9',
     meta: "{'foo': 'bar'}",
