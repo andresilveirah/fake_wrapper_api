d1dd4ec3045ac053c4265cba20571d724cbfd095
leodoin <leodoin@gmail.com>
2020-07-31 16:45:11 +0200

    add all/consent method

--- routes/all.js
+++ routes/all.js
@@ -11,11 +11,14 @@ router.post('/message-url', async (req, res) => {
   const targetingParams = JSON.stringify({ location })
   const body = {...req.body, targetingParams, alwaysDisplayDNS: false }
 
-  console.log("IP: ", ip)
-  console.log("Body to GDPR: ", body)
-  console.log("Params to CCPA: ", toQueryString(body));
+  const gdprReq = {...body, ...body.gdpr}
+  const ccpaReq = {...body, ...body.ccpa}
 
-  let [gdprResult, ccpaResult] = await Promise.all([tcfv2.getMessage(body), ccpa.getMessage(body)]);
+  console.log({ip,location})
+  console.log({gdprReq})
+  console.log({ccpaReq})
+
+  let [gdprResult, ccpaResult] = await Promise.all([tcfv2.getMessage(body), ccpa.getMessage(ccpaReq)]);
 
   if (gdprResult.err || ccpaResult.err) {
     res.status(500).json({ err: { gdpr: gdprResult.err, ccpa: ccpaResult.err }})
@@ -26,6 +29,44 @@ router.post('/message-url', async (req, res) => {
     gdpr: { ...gdprResult, gdprApplies: location === "GDPR" },
     ccpa: { ...ccpaResult, ccpaApplies: location === "CCPA" }
   })
+})
+
+router.post('/consent', async (req, res) => {
+  const ip = req.query.ip || req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+  const location = await geolookup(ip)
+  const { body } = req;
+
+  console.log({ip, location})
+  console.log({body});
+
+  let [gdprResult, ccpaResult] = await Promise.all([gdprConsent(body), ccpaConsent(body)])
+    .catch((e) => {
+      console.log(e.stack)
+      res.status(500).json({internalError: e.stack})
+      return
+    });
+
+  if (gdprResult.err || ccpaResult.err) {
+    res.status(500).json({ err: { gdpr: gdprResult.err, ccpa: ccpaResult.err }})
+    return
+  }
+
+  res.status(200).json({
+    gdpr: { ...gdprResult, gdprApplies: location === "GDPR" },
+    ccpa: { ...ccpaResult, ccpaApplies: location === "CCPA" }
+  })
+
+  async function gdprConsent(body){
+    const gdprReq = {...body, ...body.gdpr}
+    console.log({gdprReq})
+    return location === "GDPR" ? tcfv2.consent(gdprReq) : tcfv2.getMessage(gdprReq);
+  }
+
+  async function ccpaConsent(body){
+    const ccpaReq = {...body, ...body.ccpa}
+    console.log({ccpaReq})
+    return location === "CCPA" ? ccpa.consent(ccpaReq) : ccpa.getMessage(ccpaReq);
+  }
 })
 
 module.exports = router
